968247626bf4b99152d98a02805a9a1b43b1c70b
Faruque Ahamed <mhossain456@gmail.com>
2016-03-26 13:35:46 -0400

    initial commit

---
+++ src/hello/HelloWorld.java
@@ -0,0 +1,45 @@
+package hello;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("people " + "Tech " + "Astoria");
+		public int[] mergeSort (int[] array) {
+			int [] list = array:
+				if(list.length>1){
+					int[] left = lowerBoundary(int [] array);
+				int [] right =upperBoundary(arry);
+				}
+						return arry:
+					}
+		public int [] lowerBoundry (int [] array){
+			int size =array.length/2;
+			int [] left =new int[size];
+			for (int i=0; i,left.lenght; i++){
+				left[i] = array[i];
+			}
+			return left;
+		}
+		public int [] upperBoundry (int [] array){
+			int size =array.legth-arrya.length/2;
+			int [] right =new int[size];
+			for(int i=0; i<right.length; i++){
+				right[i] = array[array.length/2+i];
+			}
+			return right;
+		}
+		
+		}
+		public void merge(int [] result, int [] left, int [] right){
+			int index1 = 0;
+			int index2 = 0;
+			for (int i=0; i<result.length || (index1))
+			 
+					
+					}
+				}
+		}
+		}
+
+}
